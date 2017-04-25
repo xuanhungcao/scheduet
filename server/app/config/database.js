ed3f55a1c68f2403b20fcdc8a8237b1117571012
meodorewan <dongnguyen1308@gmail.com>
2017-04-25 12:48:25 +0700

    deploy to aws

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     localAddress  : 'mongodb://localhost/scheduet',
-    serverAddress : 'mongodb://localhost/scheduet',
+    serverAddress : 'mongodb://54.169.225.125/scheduet',
 }
