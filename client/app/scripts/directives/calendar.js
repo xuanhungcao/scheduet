90792834f13e23e5215eec8ddf0d4441fb418726
Xuan Hung <caoxuanhung96@gmail.com>
2017-04-26 17:16:15 +0700

    Add Angular-ui-calendar

--- client/app/scripts/directives/calendar.js
+++ client/app/scripts/directives/calendar.js
@@ -6,22 +6,58 @@
  * @description
  * # calendar
  */
-const name = 'calendar'
+const name = 'calendar';
 
-angular.module('app.calendar', ['ui.router'])
-  .directive('calendar', function () {
+class CalendarCtrl {
+  constructor($scope) {
+    this.eventSources = [{
+        title: 'event1',
+        start: '2017-04-20',
+        color: 'red',
+        textColor: 'white'
+    }, {
+        title: 'event2',
+        start: '2017-04-29',
+        color: 'pink',
+        textColor: 'blue'
+    }];
+
+    this.config = {
+        height: 450,
+        editable: true,
+        header:{
+            left: 'month agendaWeek agendaDay',
+            center: 'title',
+            right: 'today prev,next myButton'
+        },
+        customButtons: {
+            myButton: {
+            text:'fuck',
+            click: function() {
+                alert('oh yeahh');
+            }
+            }
+        },
+        events: this.eventSources,
+        eventClick: function(event, jsEvent, view) {
+            $scope.currentEvent = event
+        }       
+    }; 
+  }
+}
+
+angular.module('app.calendar', ['ui.router', 'ui.calendar'])
+.directive('calendar', function () {
     return {
-      templateUrl: '../views/calendar.html',
-      restrict: 'E',
-      controller: function() {
-      	this.text = 'This is calendar view';
-      },
-      controllerAs: 'calendar'
+        templateUrl: '../views/calendar.html',
+        restrict: 'E',
+        controller: CalendarCtrl,
+        controllerAs: 'calendar'
     }
-  })
-  .config(function($stateProvider) {
+})
+.config(function($stateProvider) {
   	$stateProvider.state('calendar', {
   		url: '/',
   		template: '<calendar></calendar>'
   	})
-  });
+});
