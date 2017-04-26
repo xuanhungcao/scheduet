405d3aeed6bd883767b00b2edf96623f9304df35
hainh.ownego <developers@ownego.com>
2017-04-27 00:03:34 +0700

    Register modal

--- client/app/scripts/controllers/authentication.js
+++ client/app/scripts/controllers/authentication.js
@@ -14,16 +14,55 @@ angular.module('app.navbar')
       password: ""
     };
     $scope.errorMsg = null;
+    $scope.infoMsg = null;
+
+    $scope.registerData = {
+      username: "",
+      password: "",
+      password_confirmation: ""
+    };
+
+    $scope.openLogin = () => {
+      $uibModalInstance.dismiss();
+      $uibModal.open({
+        controller: 'AuthenticationCtrl as AuthenticationCtrl',
+        templateUrl: 'views/login.html'
+      });
+    };
 
     $scope.login = () => {
       $http.post(config.serverUrl + "/login", $scope.loginData)
         .then(res => {
-          localStorage.setItem('scheduetToken', res.data.token)
+          localStorage.setItem('scheduetToken', res.data.token);
           $scope.errorMsg = null;
           $uibModalInstance.dismiss();
           $window.location.reload();
         }, res => {
           $scope.errorMsg = "Login failed, please check username or password again"
         });
-    }
+    };
+
+    $scope.openRegister = () => {
+      $uibModalInstance.dismiss();
+      $uibModal.open({
+        controller: 'AuthenticationCtrl as AuthenticationCtrl',
+        templateUrl: 'views/register.html'
+      });
+    };
+
+    $scope.register = () => {
+      if ($scope.registerData.password !== $scope.registerData.password_confirmation) {
+        $scope.errorMsg = "Password confirmation does not match, please try again";
+        return;
+      }
+      $http.post(config.serverUrl + "/register", $scope.registerData)
+        .then(res => {
+          $scope.infoMsg = "Register successfully";
+          $scope.errorMsg = null;
+        }, res => {
+          $scope.errorMsg = res.data.error;
+          $scope.infoMsg = null;
+        })
+    };
+
   });
